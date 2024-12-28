a80eb2df5ec477617563479adbf7c966324197f2
Aditya-Ingale <adityaa.ingale@gmail.com>
2024-12-28 11:32:03 +0530

    Time: 0 ms, Memory: 7.8 MB - LeetHub

--- 7031-458-231-power-of-two/7031-458-231-power-of-two.cpp
+++ 7031-458-231-power-of-two/7031-458-231-power-of-two.cpp
@@ -3,6 +3,7 @@ public:
     bool isPowerOfTwo(int n) {
             if(n <= 0)
                 return false;
+            //n & (n-1) flips the rightmost SET bit
             return (n & (n-1)) == 0;
     }
 };
