f674f400d6bb713be1fdec182ffafc057a11c0e1
Aditya Ingale <adityaa.ingale@gmail.com>
2025-03-17 20:52:01 +0530

    Time: 5 ms (30.21%), Space: 18.1 MB (49.04%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -8,12 +8,22 @@ public:
             freq[num]++;
         }
 
+        /*
+        for (unordered_map<int, int>::iterator it = freq.begin(); it != freq.end(); ++it) {
+            if (it->second % 2 != 0) {
+                return false;  // If any count is odd, return false
+            }
+        }
+        */
+
         // Check if all frequencies are even
         for (auto& entry : freq) {
             if (entry.second % 2 != 0) {
                 return false;
             }
         }
+
+        
 
         return true;
     }
