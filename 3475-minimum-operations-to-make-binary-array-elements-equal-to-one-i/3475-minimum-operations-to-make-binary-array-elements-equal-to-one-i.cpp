e97de895f415822c0af24f948efe7492e1f8aae7
Aditya Ingale <adityaa.ingale@gmail.com>
2025-03-19 14:03:55 +0530

    Time: 11 ms (31.84%), Space: 140.1 MB (47.55%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-    int n = nums.size();
-    int operations = 0;
-
-    for (int i = 0; i <= n - 3; i++) {
-        if (nums[i] == 0) {
-            // Flip nums[i], nums[i+1], nums[i+2]
-            nums[i] ^= 1;
-            nums[i+1] ^= 1;
-            nums[i+2] ^= 1;
-            operations++;
+    int n = nums.size(), ans = 0;
+    
+    for(int i = 0; i <= n - 3; i++) {  // Ensure there are 3 elements left
+        if(nums[i] == 0) {
+            ans++;
+            // Flip the triplet
+            nums[i] = 1 - nums[i];
+            nums[i+1] = 1 - nums[i+1];
+            nums[i+2] = 1 - nums[i+2];
         }
     }
 
-    // Check if the array is fully converted to all 1s
-    for (int i = 0; i < n; i++) {
-        if (nums[i] == 0) {
-            return -1;  // Impossible case
-        }
+    // Final check: If there are any remaining zeros, return -1
+    for(int i = 0; i < n; i++) {
+        if(nums[i] == 0) return -1;
     }
 
-    return operations;       
+    return ans;
     }
 };
