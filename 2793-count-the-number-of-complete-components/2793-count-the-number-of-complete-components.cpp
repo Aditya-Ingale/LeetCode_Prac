89d5beae5d5deee76f502d6cbc604dbb549fcf0f
Aditya Ingale <adityaa.ingale@gmail.com>
2025-03-22 12:55:06 +0530

    Time: 9 ms (95.78%), Space: 117.5 MB (96.55%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,48 +1,59 @@
 class Solution {
 public:
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<unordered_set<int>> adj(n);
-        vector<bool> visited(n, false);
-
-        // Build adjacency list
-        for (const auto& edge : edges) {
-            adj[edge[0]].insert(edge[1]);
-            adj[edge[1]].insert(edge[0]);
-        }
-
-        int count = 0;
-
-        // DFS to find connected components
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                vector<int> nodes;  // Store nodes in the component
-                dfs(i, adj, visited, nodes);
-
-                // Check if the component is complete
-                bool isComplete = true;
-                int size = nodes.size();
-                for (int node : nodes) {
-                    if (adj[node].size() != size - 1) { // Each node should be connected to (size-1) other nodes
-                        isComplete = false;
-                        break;
-                    }
-                }
-
-                if (isComplete) count++;
-            }
-        }
-
-        return count;
+    // Function to find the representative (parent) of a node with path compression
+    int findPar(int node, vector<int>& par) {
+        if (node == par[node])
+            return node;
+        return par[node] = findPar(par[node], par);
     }
 
-private:
-    void dfs(int node, vector<unordered_set<int>>& adj, vector<bool>& visited, vector<int>& nodes) {
-        visited[node] = true;
-        nodes.push_back(node);
-        for (int neighbor : adj[node]) {
-            if (!visited[neighbor]) {
-                dfs(neighbor, adj, visited, nodes);
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<int> count(n, 1);   // Stores the size of each component
+        vector<int> par(n);        // Stores the parent of each node
+        vector<int> edge(n, 0);    // Stores the number of edges in each component
+        vector<int> rank(n, 0);    // Helps with union by rank
+
+        // Initialize each node as its own parent (self-loop)
+        for (int i = 0; i < n; i++) {
+            par[i] = i;
+        }
+
+        // Process each edge to unite components
+        for (const auto& e : edges) {
+            int u = e[0], v = e[1];
+
+            int par1 = findPar(u, par);
+            int par2 = findPar(v, par);
+
+            if (par1 == par2) {
+                edge[par1]++; // Same component, add edge count
+            } else {
+                // Union by rank
+                if (rank[par1] == rank[par2]) {
+                    par[par1] = par2;
+                    count[par2] += count[par1];
+                    edge[par2] += edge[par1] + 1;
+                    rank[par2]++;
+                } else if (rank[par1] > rank[par2]) {
+                    par[par2] = par1;
+                    count[par1] += count[par2];
+                    edge[par1] += edge[par2] + 1;
+                } else {
+                    par[par1] = par2;
+                    count[par2] += count[par1];
+                    edge[par2] += edge[par1] + 1;
+                }
             }
         }
+
+        int res = 0;
+        // Count components that are complete graphs
+        for (int i = 0; i < n; i++) {
+            if (i == par[i] && edge[i] == (count[i] * (count[i] - 1)) / 2) {
+                res++;
+            }
+        }
+
+        return res;
     }
 };
