d89441d5d23644095ed5ed186b1a4f145e198297
Aditya Ingale <adityaa.ingale@gmail.com>
2025-04-08 18:38:22 +0530

    Time: 3 ms (55.97%), Space: 27.7 MB (54.27%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
+        unordered_set<int> seen;
         int n = nums.size();
-        int minOps = n / 3 + (n % 3 != 0); // maximum possible operations
-        for (int i = 0; i <= n; i += 3) {
-            unordered_set<int> seen;
-            bool allUnique = true;
-            for (int j = i; j < n; ++j) {
-                if (seen.count(nums[j])) {
-                    allUnique = false;
-                    break;
-                }
-                seen.insert(nums[j]);
+
+        // Start from the back of the array
+        for (int i = n - 1; i >= 0; --i) {
+            // If already seen, we must remove more from front
+            if (seen.count(nums[i])) {
+                // Number of elements before this index (i+1)
+                int remaining = i + 1;
+
+                // Number of groups of 3 we need to remove from the start
+                return (remaining + 2) / 3;
             }
-            if (allUnique) {
-                minOps = min(minOps, i / 3);
-            }
+            seen.insert(nums[i]);
         }
-        return minOps;
+
+        // If all elements are unique already
+        return 0;
     }
 };
