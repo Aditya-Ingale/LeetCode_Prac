e7f204b3d1a44f384417740c03bfca41c93ab536
Aditya Ingale <adityaa.ingale@gmail.com>
2025-04-24 13:06:18 +0530

    Time: 28 ms (35.88%), Space: 44.4 MB (34.27%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    int countCompleteSubarrays(vector<int>& nums) {
+    int countSubarraysWithKDistinct(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        int left = 0, right = 0, count = 0;
         int n = nums.size();
-        unordered_set<int> uniqueElements(nums.begin(), nums.end());
-        int totalUnique = uniqueElements.size();
-        
-        int count = 0;
-        for (int i = 0; i < n; ++i) {
-            unordered_set<int> seen;
-            for (int j = i; j < n; ++j) {
-                seen.insert(nums[j]);
-                if (seen.size() == totalUnique) {
-                    count++;
+
+        for (; right < n; ++right) {
+            freq[nums[right]]++;
+            while (freq.size() > k) {
+                freq[nums[left]]--;
+                if (freq[nums[left]] == 0) {
+                    freq.erase(nums[left]);
                 }
+                left++;
             }
+            count += (right - left + 1);
         }
+
         return count;
+    }
+
+    int countCompleteSubarrays(vector<int>& nums) {
+        unordered_set<int> unique(nums.begin(), nums.end());
+        int totalDistinct = unique.size();
+        return countSubarraysWithKDistinct(nums, totalDistinct) - 
+               countSubarraysWithKDistinct(nums, totalDistinct - 1);
     }
 };
