a1098a9180b837614c77bb78b9c8ad1534a98690
Aditya Ingale <adityaa.ingale@gmail.com>
2025-05-28 23:15:18 +0530

    Time: 111 ms (89.86%), Space: 59.2 MB (99.54%) - LeetHub

--- 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/3633-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
+++ 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/3633-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
@@ -1,61 +1,52 @@
 class Solution {
 public:
-    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
-        int n = edges1.size() + 1;
-        int m = edges2.size() + 1;
 
-        // Build trees
-        vector<vector<int>> tree1(n), tree2(m);
-        for (auto& e : edges1) {
-            tree1[e[0]].push_back(e[1]);
-            tree1[e[1]].push_back(e[0]);
-        }
-        for (auto& e : edges2) {
-            tree2[e[0]].push_back(e[1]);
-            tree2[e[1]].push_back(e[0]);
+    int DFS(int node, int par, int dist, vector<vector<int>>& edge){
+        if(dist < 0){
+            return 0;
         }
 
-        // For Tree2: count of nodes reachable from each node within distance k
-        vector<int> tree2Counts(m, 0);
-        for (int i = 0; i < m; ++i) {
-            tree2Counts[i] = bfsWithinK(tree2, i, k - 1); // -1 because edge will be added later
-        }
+        int cr = 1;
 
-        // For each node in Tree1, find best connection
-        vector<int> answer(n);
-        for (int u = 0; u < n; ++u) {
-            int tree1Reach = bfsWithinK(tree1, u, k);
-            int maxTotal = 0;
-            for (int v = 0; v < m; ++v) {
-                int combined = tree1Reach + tree2Counts[v];
-                maxTotal = max(maxTotal, combined);
+        for(auto adj : edge[node]){
+            if(adj != par){
+                cr += DFS(adj, node, dist-1, edge);
             }
-            answer[u] = maxTotal;
         }
 
-        return answer;
+        return cr;
     }
+    
+    vector<int> maxTargetNodes(vector<vector<int>>& e1, vector<vector<int>>& e2, int k) {
+        int n = e1.size()+1;
+        int m = e2.size()+1;
+        
+        vector<vector<int>> edge1(n);
+        vector<vector<int>> edge2(m);
 
-private:
-    int bfsWithinK(const vector<vector<int>>& tree, int start, int k) {
-        int count = 0;
-        int n = tree.size();
-        vector<bool> visited(n, false);
-        queue<pair<int, int>> q;
-        q.push({start, 0});
-        visited[start] = true;
-
-        while (!q.empty()) {
-            auto [node, dist] = q.front(); q.pop();
-            if (dist > k) continue;
-            count++;
-            for (int nei : tree[node]) {
-                if (!visited[nei]) {
-                    visited[nei] = true;
-                    q.push({nei, dist + 1});
-                }
-            }
+        for(int i=0; i<n-1; i++){
+            edge1[e1[i][0]].push_back(e1[i][1]);
+            edge1[e1[i][1]].push_back(e1[i][0]);
         }
-        return count;
+
+        for(int i=0; i<m-1; i++){
+            edge2[e2[i][0]].push_back(e2[i][1]);
+            edge2[e2[i][1]].push_back(e2[i][0]);
+        }
+
+        int mxIn2 = 0;
+        for(int i=0; i<m; i++){
+            int ifInode = DFS(i,-1,k-1,edge2);
+            mxIn2 = max(mxIn2, ifInode);
+        }
+
+        vector<int> ans(n);
+
+        for(int i=0; i<n; i++){
+            int cur = DFS(i,-1,k,edge1);
+            ans[i] = cur + mxIn2;
+        }
+
+        return ans;
     }
 };
