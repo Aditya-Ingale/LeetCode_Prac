56fb116b4a3406b365408279809a86c7d355e981
Aditya Ingale <adityaa.ingale@gmail.com>
2025-06-07 21:55:41 +0530

    Time: 63 ms (75.65%), Space: 26.2 MB (66.23%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,32 +1,21 @@
 class Solution {
-public:
-    string clearStars(string s) {
-        vector<char> stk;
+ public:
+  string clearStars(string s) {
+    string ans = s;
+    vector<vector<int>> buckets(26);
 
-        for (char ch : s) {
-            if (ch == '*') {
-                // Find index of smallest character from right to left
-                int minIndex = -1;
-                char minChar = 'z' + 1;
+    for (int i = 0; i < s.length(); ++i)
+      if (s[i] == '*') {
+        ans[i] = ' ';
+        int j = 0;
+        while (buckets[j].empty())
+          ++j;
+        ans[buckets[j].back()] = ' ', buckets[j].pop_back();
+      } else {
+        buckets[s[i] - 'a'].push_back(i);
+      }
 
-                // Find the smallest character in the stack
-                for (int i = stk.size() - 1; i >= 0; --i) {
-                    if (stk[i] != '*') {
-                        if (stk[i] < minChar) {
-                            minChar = stk[i];
-                            minIndex = i;
-                        }
-                    }
-                }
-
-                if (minIndex != -1) {
-                    stk.erase(stk.begin() + minIndex);
-                }
-            } else {
-                stk.push_back(ch);
-            }
-        }
-
-        return string(stk.begin(), stk.end());
-    }
+    std::erase(ans, ' ');
+    return ans;
+  }
 };
