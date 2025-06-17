81987939d31490a9fcbbf19682dd7198eeaf32cf
Aditya Ingale <adityaa.ingale@gmail.com>
2025-06-17 13:24:02 +0530

    Time: 0 ms (100%), Space: 10.2 MB (78.41%) - LeetHub

--- 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements.cpp
+++ 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements.cpp
@@ -1,42 +1,43 @@
-class Solution {
-public:
-    const int MOD = 1e9 + 7;
-    const int MAX = 1e5 + 5;
-    vector<long long> fact, invFact;
+const int MOD = 1e9 + 7;
+const int MX = 1e5;
 
-    long long modPow(long long a, long long b) {
+long long fact[MX];
+long long inv_fact[MX];
+
+class Solution {
+    long long qpow(long long x, int n) {
         long long res = 1;
-        while (b) {
-            if (b & 1) res = (res * a) % MOD;
-            a = (a * a) % MOD;
-            b >>= 1;
+        while (n) {
+            if (n & 1) {
+                res = res * x % MOD;
+            }
+            x = x * x % MOD;
+            n >>= 1;
         }
         return res;
     }
 
-    void precomputeFactorials() {
-        fact.resize(MAX);
-        invFact.resize(MAX);
+    long long comb(int n, int m) {
+        return fact[n] * inv_fact[m] % MOD * inv_fact[n - m] % MOD;
+    }
+    void init() {
+        if (fact[0]) {
+            return;
+        }
         fact[0] = 1;
-        for (int i = 1; i < MAX; ++i) {
-            fact[i] = (fact[i - 1] * i) % MOD;
+        for (int i = 1; i < MX; i++) {
+            fact[i] = fact[i - 1] * i % MOD;
         }
-        invFact[MAX - 1] = modPow(fact[MAX - 1], MOD - 2);
-        for (int i = MAX - 2; i >= 0; --i) {
-            invFact[i] = (invFact[i + 1] * (i + 1)) % MOD;
+
+        inv_fact[MX - 1] = qpow(fact[MX - 1], MOD - 2);
+        for (int i = MX - 1; i; i--) {
+            inv_fact[i - 1] = inv_fact[i] * i % MOD;
         }
     }
 
-    long long comb(int n, int r) {
-        if (r < 0 || r > n) return 0;
-        return (((fact[n] * invFact[r]) % MOD) * invFact[n - r]) % MOD;
-    }
-
+public:
     int countGoodArrays(int n, int m, int k) {
-        precomputeFactorials();
-        long long ways = comb(n - 1, k);                      // choose positions for equality
-        ways = (ways * m) % MOD;                              // first element choice
-        ways = (ways * modPow(m - 1, n - 1 - k)) % MOD;       // transitions
-        return ways;
+        init();
+        return comb(n - 1, k) * m % MOD * qpow(m - 1, n - k - 1) % MOD;
     }
 };
