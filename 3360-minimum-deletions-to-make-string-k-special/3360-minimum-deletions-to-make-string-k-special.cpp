dce228102b2d767be6ea01443996cd26c21da491
Aditya Ingale <adityaa.ingale@gmail.com>
2025-06-21 21:46:22 +0530

    Time: 9 ms (51.81%), Space: 19.4 MB (55.07%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
-    int minimumDeletions(string word, int k) {
-        unordered_map<char, int> freq;
-        for (char c : word) {
-            freq[c]++;
+   int minimumDeletions(string word, int k) {
+    vector<int> freq(26, 0);
+    int deleted = 0, ans = word.size();
+    for(auto c: word) freq[c-'a']++;             // find frequency of each chars
+    sort(freq.begin(), freq.end());              // sort the freq array
+    for(int i = 0; i < freq.size(); ++i){        // Iterate over freq 
+        int res = deleted, minFreq = freq[i];    // consider the current freq as the min freq after deletion. Also add the already deleted freq to temporary result
+        for(int j = freq.size()-1; j > i; --j){  // iterate over freq array to evaluate the chars to be deleted from the higher freq side of the freq array
+            if(freq[j] - minFreq <= k) break;    // Once condition is satisfied then exit from the loop
+            res += freq[j] - minFreq - k;        // accumulate on the result - number of items to be deleted
         }
-
-        vector<int> freqs;
-        for (auto& [ch, f] : freq) {
-            freqs.push_back(f);
-        }
-
-        int res = INT_MAX;
-        int max_freq = *max_element(freqs.begin(), freqs.end());
-
-        for (int target = 1; target <= max_freq; ++target) {
-            int deletions = 0;
-            for (int f : freqs) {
-                if (f > target + k) {
-                    deletions += f - (target + k);  // Reduce frequency
-                } else if (f < target) {
-                    deletions += f;  // Delete entire character group
-                }
-                // else: in range [target, target + k] => no deletion needed
-            }
-            res = min(res, deletions);
-        }
-
-        return res;
+        ans = min(ans, res);                     // keep tracking the min possible value
+        deleted += freq[i];                      // update the deleted freq with current freq
     }
+    return ans;
+}
 };
