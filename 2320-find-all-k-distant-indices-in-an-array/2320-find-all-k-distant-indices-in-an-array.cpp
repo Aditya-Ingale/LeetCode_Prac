013d99962115e44f92ad9fdf0572fe03ca018a46
Aditya Ingale <adityaa.ingale@gmail.com>
2025-06-24 23:01:12 +0530

    Time: 0 ms (100%), Space: 14.7 MB (55.05%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cpp
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
         int n = nums.size();
-        vector<bool> isKDist(n, false);  // to mark k-distant indices
-
+        vector<int> keyIndices;
         for (int i = 0; i < n; ++i) {
-            if (nums[i] == key) {
-                // mark from max(0, i - k) to min(n - 1, i + k)
-                int start = max(0, i - k);
-                int end = min(n - 1, i + k);
-                for (int j = start; j <= end; ++j) {
-                    isKDist[j] = true;
-                }
-            }
+            if (nums[i] == key) keyIndices.push_back(i);
         }
 
         vector<int> result;
+        int j = 0;  // pointer for keyIndices
+
         for (int i = 0; i < n; ++i) {
-            if (isKDist[i]) result.push_back(i);
+            // Move j forward until keyIndices[j] is at least i - k
+            while (j < keyIndices.size() && keyIndices[j] < i - k) {
+                ++j;
+            }
+            // If current key index is within distance k
+            if (j < keyIndices.size() && abs(keyIndices[j] - i) <= k) {
+                result.push_back(i);
+            }
         }
 
         return result;
